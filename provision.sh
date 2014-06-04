3c2544ae7f3e9eab8873695e2e70754db9014ced
George Miroshnykov <george.miroshnykov@gmail.com>
2014-06-04 15:30:54 +0300

    Remove obsolete files and update config.example.json

--- provision.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-sudo apt-get update
-sudo apt-get install -y vim nginx python-dev python-pip
-sudo ln -sf /opt/proxxy/nginx.conf /etc/nginx/sites-enabled/default
-sudo service nginx restart
