b16aae9b60583c033ed5bffb7fdd65d4e610469c
George Miroshnykov <george.miroshnykov@gmail.com>
2014-07-10 10:27:53 +0300

    Config path fix

--- packer/proxxy.sh
+++ packer/proxxy.sh
@@ -4,6 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
 apt-get install -y git
 git clone https://github.com/laggyluke/proxxy.git /opt/proxxy
-cp /opt/packer/config.json /opt/proxxy/config.json
+cp /opt/packer/config.json /opt/proxxy/app/config.json
 docker build --rm --tag laggyluke/proxxy /opt/proxxy
 docker run --name proxxy --detach --publish=80:80 laggyluke/proxxy
