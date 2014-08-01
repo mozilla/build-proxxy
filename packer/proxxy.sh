1ab80ba7d808b92bb95a21da3e9b6215bef4edcb
George Miroshnykov <george.miroshnykov@gmail.com>
2014-08-01 18:39:42 +0300

    Mount cache and logs inside /mnt

--- packer/proxxy.sh
+++ packer/proxxy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 mkdir -p /opt/proxxy
+mkdir -p /mnt/proxxy
 mv /opt/packer/app /opt/proxxy/app
 chmod +x /opt/proxxy/app/*.sh
 docker build --rm --tag proxxy /opt/proxxy/app
-docker run --name proxxy --detach --publish=80:80 proxxy
+docker run --name proxxy \
+  --detach \
+  --publish=80:80 \
+  --volume=/mnt/proxxy/cache:/var/cache/proxxy \
+  --volume=/mnt/proxxy/logs:/var/log/proxxy \
+  proxxy
